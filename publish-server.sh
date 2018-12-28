ed507f37cc1ecb27c6020c2f04cab05db138d278
Rodrigo Souza <rsouza01@gmail.com>
2018-12-28 18:55:05 -0200

    Build script renamed

--- publish-server.sh
+++ publish-server.sh
@@ -9,31 +9,31 @@ LOGIN_NAME=rsouza01
 IMG_TAG=latest
 
 echo "------------------------------------------------------------------"
-echo "Running maven."
+echo "Running maven for $SERVER_NAME."
 echo "------------------------------------------------------------------"
 
-#$MAVEN package -f pom.xml -DskipTests
+$MAVEN package -f pom.xml -DskipTests
 
 echo "------------------------------------------------------------------"
-echo "Copying jar."
+echo "Copying $SERVER_NAME.jar."
 echo "------------------------------------------------------------------"
 
-#cp ./target/$SERVER_NAME-0.0.1-SNAPSHOT.jar ./jar/$SERVER_NAME.jar
+cp ./target/$SERVER_NAME-0.0.1-SNAPSHOT.jar ./jar/$SERVER_NAME.jar
 
 echo "------------------------------------------------------------------"
-echo "Deleting container."
+echo "Deleting container $ORG_NAME/$SERVER_NAME:$IMG_TAG."
 echo "------------------------------------------------------------------"
 
 sudo docker rm  $ORG_NAME/$SERVER_NAME:$IMG_TAG
 
 echo "------------------------------------------------------------------"
-echo "Dockerizing."
+echo "Dockerizing $ORG_NAME/$SERVER_NAME:$IMG_TAG."
 echo "------------------------------------------------------------------"
 
 sudo docker build --tag $ORG_NAME/$SERVER_NAME:$IMG_TAG --file ./dockerfiles/Dockerfile.server.yml --rm=true .
 
 echo "------------------------------------------------------------------"
-echo "Sending to Docker Hub"
+echo "Sending $ORG_NAME/$SERVER_NAME:$IMG_TAG to Docker Hub"
 echo "------------------------------------------------------------------"
 
 sudo docker login --username=$LOGIN_NAME
@@ -44,7 +44,7 @@ echo "------------------------------------------------------------------"
 echo "Running docker image."
 echo "------------------------------------------------------------------"
 
-sudo docker run --name=$SERVER_NAME --publish=$SERVER_PORT:$SERVER_PORT -it $SERVER_NAME:latest
+#sudo docker run --name=$SERVER_NAME --publish=$SERVER_PORT:$SERVER_PORT -it $SERVER_NAME:latest
 
 echo "------------------------------------------------------------------"
 echo "Done!"
